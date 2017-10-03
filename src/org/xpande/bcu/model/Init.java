742f0e069247e6a58f55aec2226f37a3c5db246b
gvilauy <gabrielvila13@gmail.com>
2017-10-03 13:10:04 -0300

    Initial commit

---
+++ src/org/xpande/bcu/model/Init.java
@@ -0,0 +1,9 @@
+package org.xpande.bcu.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 10/3/17.
+ */
+public class Init {
+
+}
